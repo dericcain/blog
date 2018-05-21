95cc6347d7308fd1bf79e196a46279d94515b673
Deric Cain <dcain@guidewire.com>
2018-05-21 14:27:44 -0500

    [WIP] Updated function params

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -1,17 +1,12 @@
 const API_KEY = process.env.MAILGUN_API_KEY;
 const URL = `https://api:${API_KEY}@api.mailgun.net/v3/subscribe.dericcain.com/lists/followers@subscribe.dericcain.com/members`;
 
-exports.handler = function ({ body: { payload, site }, isBase64Encoded }, context, callback) {
-  console.log(payload);
-  console.log(URL);
-  console.log(isBase64Encoded);
-  const { email } = payload;
-  if (email) {
-    console.log(email);
-    console.log(site);
-  }
+exports.handler = (event, context, callback) => {
+  console.log(event);
+  console.log(context);
+
   callback(null, {
     statusCode: 200,
     body: 'Done!'
   });
-}
+};
