6ae756c22d960aa1f638bc9fa877e522275513b0
Deric Cain <dcain@guidewire.com>
2018-05-21 14:48:17 -0500

    [WIP] checking body

--- src/raw-functions/submission-created.js
+++ src/raw-functions/submission-created.js
@@ -1,13 +1,17 @@
 const API_KEY = process.env.MAILGUN_API_KEY;
 const URL = `https://api:${API_KEY}@api.mailgun.net/v3/subscribe.dericcain.com/lists/followers@subscribe.dericcain.com/members`;
 
-exports.handler = (event, context, callback) => {
-  console.log(event);
+exports.handler = ({ body }, context, callback) => {
+  console.log(body);
 
-  // fetch({
-  //   url: URL,
-  //
-  // })
+  const email = '';
+
+  fetch(URL, {
+    method: 'POST',
+    body: JSON.stringify({ email }),
+  }).then(result => {
+
+  });
 
   callback(null, {
     statusCode: 200,
