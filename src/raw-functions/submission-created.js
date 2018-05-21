eff377a73e487a09cecfdc296b0d6d910cabbe1e
Deric Cain <dcain@guidewire.com>
2018-05-21 14:58:10 -0500

    [WIP] console email

--- src/raw-functions/submission-created.js
+++ src/raw-functions/submission-created.js
@@ -2,7 +2,7 @@ const API_KEY = process.env.MAILGUN_API_KEY;
 const URL = `https://api:${API_KEY}@api.mailgun.net/v3/subscribe.dericcain.com/lists/followers@subscribe.dericcain.com/members`;
 
 exports.handler = ({ body }, context, callback) => {
-  console.log(body.payload.email);
+  console.log(body.payload);
 
   const email = body.payload.email;
 
