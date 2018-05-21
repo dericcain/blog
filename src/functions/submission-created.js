6a32508dec03390187ae720cca68300208b96902
Deric Cain <dcain@guidewire.com>
2018-05-21 14:38:25 -0500

    [WIP] Removed context log

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -1,12 +1,13 @@
 const API_KEY = process.env.MAILGUN_API_KEY;
 const URL = `https://api:${API_KEY}@api.mailgun.net/v3/subscribe.dericcain.com/lists/followers@subscribe.dericcain.com/members`;
 
-exports.handler = (event, context, callback) => {
+export const handler = (event, context, callback) => {
   console.log(event);
-  console.log(context);
 
   callback(null, {
     statusCode: 200,
     body: 'Done!'
   });
 };
+
+
