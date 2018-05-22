db6cf82de123e890664acb40b1f3ecc90584084e
Deric Cain <dcain@guidewire.com>
2018-05-22 10:51:27 -0500

    [WIP] Form submission flow

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -105,8 +105,8 @@ Object.defineProperty(exports, "__esModule", {
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-const mailgun = __webpack_require__(113)({ apiKey: API_KEY, domain: 'subscribe.dericcain.com' });
 const API_KEY = process.env.MAILGUN_API_KEY;
+const mailgun = __webpack_require__(113)({ apiKey: API_KEY, domain: 'subscribe.dericcain.com' });
 
 const handler = exports.handler = (() => {
   var _ref = _asyncToGenerator(function* (event, context, callback) {
