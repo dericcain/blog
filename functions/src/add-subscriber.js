3a7351f37d18e03f9c74bd8facd314da6bc195a3
dericgw <dcain@guidewire.com>
2018-06-03 07:17:45 -0500

    Added messages for debugging

--- functions/src/add-subscriber.js
+++ functions/src/add-subscriber.js
@@ -3,22 +3,29 @@ const mailgun = require('mailgun-js')({ apiKey: API_KEY, domain: 'subscribe.deri
 
 export const handler = async (event, context, callback) => {
   if (event.httpMethod !== 'POST') {
+    console.log('Non-POST event');
     callback(null, {
       statusCode: 200,
     });
   }
-
+  
   const { email } = JSON.parse(event.body);
   const list = mailgun.lists(`followers@subscribe.dericcain.com`);
-
-  console.log(email);
   
-
-  list.members().create({ address: email }, (error, data) => {
+  console.log(`The email is: ${email}`);
+  const subscribed = {
+    address: email,
+    subscribed: true,
+  }
+  list.members().create(subscribed, (error, data) => {
     if (error) {
-      callback(error);
+      console.log(`Something happened: ${error}`);
+      callback(error, {
+        statusCode: 400
+      });
     }
 
+    console.log(`Email was added: ${JSON.stringify(data)}`)
     callback(null, {
       statusCode: 200,
       body: JSON.stringify(data),
